6e8ecfb63f23624db313a1b55e6aaa10ec5968fb
Atsz7 <angeltsz74@gmail.com>
2021-10-31 17:19:55 -0700

    Testing notifications

--- app/src/main/java/com/app/atsz7/pushcore/MainActivity.kt
+++ app/src/main/java/com/app/atsz7/pushcore/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.app.atsz7.pushcore
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import com.app.atsz7.pushcore.core.ui.activities.BaseActivity
+import com.app.atsz7.pushcore.help.ui.HelpActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getFirebaseToken()
+
+        findViewById<Button>(R.id.btnAction).setOnClickListener {
+            startActivity(Intent(this, HelpActivity::class.java))
+        }
     }
 
     private fun getFirebaseToken() {
